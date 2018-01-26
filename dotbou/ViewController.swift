504e581201abd316f6eaf8cd0c7745dfe07640c5
ママ <ak.08047045654@softbank.ne.jp>
2018-01-26 12:11:26 +0900

    Initial Commit

---
+++ dotbou/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dotbou
+//
+//  Created by 藤田佳恵 on 2018/01/26.
+//  Copyright © 2018年 藤田佳恵. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
